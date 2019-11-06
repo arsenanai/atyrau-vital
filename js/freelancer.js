386b912adabad8ac7b13c753ef1e2e2643dd18db
Arsen Anay <arsenanai@gmail.com>
2019-11-06 11:32:02 +0600

    menu changes

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,18 +1,8 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 70)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500
   });
 
   // Scroll to top button appear
